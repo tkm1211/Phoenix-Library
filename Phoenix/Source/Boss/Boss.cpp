dd20b326b6c0aedcf26bee12ccf352015de57dc5
tkm1211 <tkm.1239@gmail.com>
2021-02-01 16:46:46 +0900

    前回のボスコード削除

--- Phoenix/Source/Boss/Boss.cpp
+++ Phoenix/Source/Boss/Boss.cpp
@@ -3,454 +3,6 @@
 #include "../Enemy/EnemyManager.h"
 
 
-//std::unique_ptr<Boss> Boss::Create()
-//{
-//	return std::make_unique<Boss>();
-//}
-//
-//void Boss::Construct(Phoenix::Graphics::IGraphicsDevice* graphicsDevice, Player* player)
-//{
-//	// モデル読み込み
-//	{
-//		model = std::make_unique<Phoenix::FrameWork::ModelObject>();
-//		model->Initialize(graphicsDevice);
-//		model->Load(graphicsDevice, "..\\Data\\Assets\\Model\\Boss\\Mutant\\Idle\\Mutant_Roaring.fbx"); // Mutant_Idle02 Mutant_Breathing_Idle Mutant_Roaring
-//
-//		effectModel = std::make_unique<Phoenix::FrameWork::ModelObject>();
-//		effectModel->Initialize(graphicsDevice);
-//		effectModel->Load(graphicsDevice, "..\\Data\\Assets\\Model\\Effect\\BossEffect\\JumpAttackEffect01.fbx");
-//	}
-//
-//	// アニメーション読み込み
-//	{
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Run\\Mutant_Run.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Avoid\\Sprinting_Forward_Roll.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Attack\\Right\\Mutant_Swiping.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Attack\\Left\\Mutant_Punch.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Attack\\Rotate\\Right_Rotate.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Attack\\Jump\\Jump_Attack02.fbx", -1);
-//		model->LoadAnimation("..\\Data\\Assets\\Model\\Boss\\Mutant\\Damage\\Head_Hit.fbx", -1);
-//
-//		model->AddAnimationLayer(0);
-//		model->AddAnimationLayer(1);
-//		model->AddAnimationLayer(2);
-//		model->AddAnimationLayer(3);
-//		model->AddAnimationLayer(4);
-//		model->AddAnimationLayer(5);
-//		model->AddAnimationLayer(6);
-//		model->AddAnimationLayer(7);
-//
-//		effectModel->AddAnimationLayer(0);
-//	}
-//
-//	// コリジョン初期化
-//	{
-//		collisionDatas.resize(4);
-//
-//		collisionDatas.at(0).pos = Phoenix::Math::Vector3(0.0f, 0.0f, 0.0f);
-//		collisionDatas.at(0).radius = 1.5f;
-//		collisionDatas.at(0).boneIndex = model->GetBoneIndex("Mutant:Hips");
-//
-//		collisionDatas.at(1).pos = Phoenix::Math::Vector3(0.0f, 0.0f, 0.0f);
-//		collisionDatas.at(1).radius = 0.5f;
-//		collisionDatas.at(1).boneIndex = model->GetBoneIndex("Mutant:RightHandIndex1");
-//
-//		collisionDatas.at(2).pos = Phoenix::Math::Vector3(0.0f, 0.0f, 0.0f);
-//		collisionDatas.at(2).radius = 1.3f;
-//		collisionDatas.at(2).boneIndex = model->GetBoneIndex("Mutant:LeftHand");
-//
-//		collisionDatas.at(3).pos = Phoenix::Math::Vector3(0.0f, 0.0f, 0.0f);
-//		collisionDatas.at(3).radius = 2.65f;
-//		collisionDatas.at(3).boneIndex = -1;
-//	}
-//
-//	// プレイヤーアドレス取得
-//	{
-//		this->player = player;
-//	}
-//
-//	// UI生成
-//	{
-//		ui = BossUI::Create();
-//	}
-//
-//	// AI初期化
-//	{
-//		ai = BossAI::Create();
-//		ai->Construct();
-//
-//		bossAI = static_cast<BossAI*>(ai.get());
-//		bossAI->SharedBossPtr(this);
-//		bossAI->SharedPlayerPtr(player);
-//	}
-//}
-//
-//void Boss::Initialize()
-//{
-//	// 待機モーション開始
-//	{
-//		model->PlayAnimation(0, 0, 1);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		model->SetSpeed(2.0f);
-//		//model->PauseAnimation(true);
-//	}
-//
-//	// トランスフォームの初期化
-//	{
-//		worldMatrix = Phoenix::Math::MatrixIdentity();
-//		pos = { 0,0,-12.0f }; // test : 100.0f
-//		//rotate = { 0,0,0 };
-//		rotate = { 0,0,0,1 };
-//		scale = { 2.0f,2.0f,2.0f };
-//		//scale = { 1.0f,1.0f,1.0f };
-//		radius = 0.75f;
-//		life = MaxLife;
-//		accumulationDamege = 0;
-//		accumulationTimeCnt = 0;
-//		isChangeAccumulationDamege = false;
-//	}
-//
-//	// AI初期化
-//	{
-//		ai->Initialize();
-//		currentType = bossAI->GetCurrentStateType();
-//	}
-//
-//	// インデックス取得
-//	{
-//		boneIndex = model->GetBoneIndex("Mutant:Hips");
-//	}
-//
-//	isDissolve = false;
-//	isJumpAttackStart = false;
-//	onJumpAttackStart = false;
-//}
-//
-//void Boss::Update(bool onControl)
-//{
-//	// 蓄積ダメージの確認
-//	{
-//		AccumulationDamege();
-//	}
-//
-//	// ステートタイプ変更 (アニメーション変更)
-//	if (nextType != AIStateType::None)
-//	{
-//		currentType = nextType;
-//		nextType = AIStateType::None;
-//		ChangeAnimation(currentType);
-//	}
-//
-//	// AI更新
-//	{
-//		if (onControl) ai->Update();
-//	}
-//
-//	// 次のステートタイプを取得
-//	{
-//		nextType = bossAI->GetNextStateType();
-//	}
-//
-//	// アニメーション更新
-//	{
-//		model->UpdateTransform(1 / 60.0f);
-//
-//#if 0
-//		// TODO : リファクタリング (アニメーション時間の取得)
-//		if (!isDissolve) effectModel->UpdateTransform(1 / 60.0f);
-//		if (IsJumpAttack())
-//		{
-//			if (animCnt <= 150.0f / 60.0f)
-//			{
-//				isDissolve = false;
-//			}
-//			else
-//			{
-//				isDissolve = true;
-//			}
-//			animCnt += 1.0f / 60.0f;
-//		}
-//		else
-//		{
-//			animCnt = 0.0f;
-//			isDissolve = false;
-//		}
-//#else
-//		if (IsJumpAttack()) effectModel->UpdateTransform(1 / 60.0f);
-//#endif
-//	}
-//
-//	// ワールド行列を作成
-//	{
-//		//pos.y -= 9.8f * 0.01f;
-//		//if (pos.y <= 0.0f) pos.y = 0.0f;
-//
-//		Phoenix::Math::Matrix S, R, T;
-//		S = Phoenix::Math::MatrixScaling(scale.x, scale.y, scale.z);
-//		//R = Phoenix::Math::MatrixRotationRollPitchYaw(rotate.x, rotate.y, rotate.z);
-//		R = Phoenix::Math::MatrixRotationQuaternion(&rotate);
-//		T = Phoenix::Math::MatrixTranslation(pos.x, pos.y, pos.z);
-//		worldMatrix = S * R * T;
-//	}
-//
-//	// コリジョン更新
-//	{
-//		Phoenix::Math::Matrix systemUnitTransform = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
-//		systemUnitTransform._11 = systemUnitTransform._22 = systemUnitTransform._33 = 0.01f;
-//
-//		auto nodes = model->GetNodes();
-//		for (auto& data : collisionDatas)
-//		{
-//			if (data.boneIndex == -1)
-//			{
-//				data.pos = pos;
-//				continue;
-//			}
-//			Phoenix::Math::Matrix bone = nodes->at(data.boneIndex).worldTransform;
-//			bone *= systemUnitTransform * worldMatrix;
-//			data.pos = Phoenix::Math::Vector3(bone._41, bone._42, bone._43);
-//		}
-//	}
-//
-//	// アタック判定中
-//	{
-//		AttackJudgment();
-//	}
-//}
-//
-//void Boss::AccumulationDamege()
-//{
-//	if (accumulationDamege == 0) return;
-//
-//	if (AccumulationMaxDamege <= accumulationDamege)
-//	{
-//		isChangeAccumulationDamege = true;
-//		nextType = AIStateType::Damage;
-//
-//		accumulationDamege = 0;
-//		accumulationTimeCnt = 0;
-//	}
-//	else if (AccumulationTime <= accumulationTimeCnt++)
-//	{
-//		accumulationDamege = 0;
-//		accumulationTimeCnt = 0;
-//	}
-//}
-//
-//void Boss::UpdateUI()
-//{
-//	Phoenix::f32 hp = static_cast<Phoenix::f32>(life);
-//	hp = hp <= 0 ? 0 : hp;
-//
-//	ui->Update((hp / MaxLife) * 100.0f);
-//}
-//
-//void Boss::ChangeAnimation(AIStateType type)
-//{
-//	switch (type)
-//	{
-//	case AIStateType::Wait:
-//		model->PlayAnimation(0, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		model->SetSpeed(2.0f);
-//		break;
-//
-//	case AIStateType::Move:
-//		model->PlayAnimation(1, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(true);
-//		break;
-//
-//	case AIStateType::Avoid:
-//		model->PlayAnimation(2, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		model->SetSpeed(1.5f);
-//		break;
-//
-//	case AIStateType::SwingAttack01:
-//		model->PlayAnimation(3, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		break;
-//
-//	case AIStateType::SwingAttack02:
-//		model->PlayAnimation(4, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		break;
-//
-//	case AIStateType::RotationAttack:
-//		model->PlayAnimation(5, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		break;
-//
-//	case AIStateType::JumpAttack:
-//		model->PlayAnimation(6, 0, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//
-//		effectModel->PlayAnimation(0, 0, 0.2f);
-//		effectModel->UpdateTransform(1 / 60.0f);
-//		effectModel->SetLoopAnimation(false);
-//		//effectModel->SetEndTime(160.0f / 60.0f);
-//		break;
-//
-//	case AIStateType::Damage:
-//		model->PlayAnimation(7, 1, 0.2f);
-//		model->UpdateTransform(1 / 60.0f);
-//		model->SetLoopAnimation(false);
-//		break;
-//
-//	case AIStateType::None:
-//		break;
-//
-//	default: break;
-//	}
-//}
-//
-//void Boss::AttackJudgment()
-//{
-//	auto Judgment = [&](Phoenix::s32 index)
-//	{
-//		if (isHit)
-//		{
-//			isAttackJudgment = false;
-//			return;
-//		}
-//
-//		isAttackJudgment = true;
-//		attackCollisionIndex = index;
-//	};
-//	auto NoJudgment = [&]()
-//	{
-//		isAttackJudgment = false;
-//		isHit = false;
-//		attackCollisionIndex = -1;
-//	};
-//
-//	if (currentType == AIStateType::SwingAttack01)
-//	{
-//		BossAI* bossAI = static_cast<BossAI*>(ai.get());
-//		float time = static_cast<SwingAttackState01*>(bossAI->GetCurrentState())->GetAnimationCnt() * 60.0f;
-//
-//		if (76.0f <= time && time <= 90.0f)
-//		{
-//			Judgment(2);
-//		}
-//		else
-//		{
-//			NoJudgment();
-//		}
-//	}
-//	else if (currentType == AIStateType::SwingAttack02)
-//	{
-//		BossAI* bossAI = static_cast<BossAI*>(ai.get());
-//		float time = static_cast<SwingAttackState02*>(bossAI->GetCurrentState())->GetAnimationCnt() * 60.0f;
-//
-//		if (14.0f <= time && time <= 23.0f)
-//		{
-//			Judgment(1);
-//		}
-//		else
-//		{
-//			NoJudgment();
-//		}
-//	}
-//	else if (currentType == AIStateType::RotationAttack)
-//	{
-//		BossAI* bossAI = static_cast<BossAI*>(ai.get());
-//		float time = static_cast<RotationAttackState*>(bossAI->GetCurrentState())->GetAnimationCnt() * 60.0f;
-//
-//		if (37.0f <= time && time <= 77.0f)
-//		{
-//			Judgment(2);
-//		}
-//		else
-//		{
-//			NoJudgment();
-//		}
-//	}
-//	else if (currentType == AIStateType::JumpAttack)
-//	{
-//		BossAI* bossAI = static_cast<BossAI*>(ai.get());
-//		float time = static_cast<JumpAttackState*>(bossAI->GetCurrentState())->GetAnimationCnt() * 60.0f;
-//
-//		//if (37.0f <= time && time <= 50.0f)
-//		if (102.0f <= time && time <= 122.0f)
-//		{
-//			Judgment(3);
-//
-//			isJumpAttackStart = false;
-//			if (!onJumpAttackStart)
-//			{
-//				isJumpAttackStart = true;
-//				onJumpAttackStart = true;
-//			}
-//		}
-//		else
-//		{
-//			NoJudgment();
-//			onJumpAttackStart = false;
-//		}
-//	}
-//}
-//
-//void Boss::GUI()
-//{
-//	if (ImGui::TreeNode("Boss"))
-//	{
-//		if (ImGui::TreeNode("Prameter"))
-//		{
-//			ImGui::DragFloat3("pos", &pos.x);
-//			ImGui::Text("HP : %d", life);
-//			ImGui::TreePop();
-//		}
-//		ai->GUI();
-//		if (ImGui::TreeNode("Player data as seen by the boss"))
-//		{
-//			Phoenix::Math::Vector3 pos = player->GetPosition();
-//			ImGui::Text("Pos : %f, %f, %f", pos.x, pos.y, pos.z);
-//			ImGui::TreePop();
-//		}
-//		if (ImGui::TreeNode("Bone"))
-//		{
-//			/*for (auto& node : *model->GetNodes())
-//			{
-//				if (node.parent == nullptr)
-//				{
-//					ImGui::Text(node.name);
-//					ImGui::Text("translate : %f, %f, %f", node.translate.x, node.translate.y, node.translate.z);
-//				}
-//			}*/
-//
-//			if (ImGui::TreeNode("BoneName"))
-//			{
-//				//ImGui::ListBox("BoneListBox\n(single select)", &boneIndex, model->GetBoneNode()->at(0).name.data(), model->GetBoneNode()->at(0).name.size(), 4);
-//				//ImGui::ListBox("NodeListBox\n(single select)", &nodeIndex, model->GetNodes()->data(), model->GetNodes()->size(), 4);
-//				Phoenix::s32 index = 0;
-//				ImGui::ListBox("ListBox\n(single select)", &index, model->GetBoneNames().data(), static_cast<Phoenix::s32>(model->GetBoneNames().size()), 4);
-//				ImGui::TreePop();
-//			}
-//
-//			Phoenix::Math::Matrix boneM = model->GetBoneTransforms(0, boneIndex);
-//			Phoenix::Math::Matrix mat = boneM * worldMatrix;
-//			Phoenix::Math::Matrix matI = boneM * Phoenix::Math::MatrixInverse(worldMatrix);
-//
-//			ImGui::Text("wolrd translate : %f, %f, %f", worldMatrix._41, worldMatrix._42, worldMatrix._43);
-//			ImGui::Text("bone translate : %f, %f, %f", boneM._41, boneM._42, boneM._43);
-//			ImGui::Text("bone translate * worldMatrix : %f, %f, %f", mat._41, mat._42, mat._43);
-//			ImGui::Text("bone translate * Inverse(worldMatrix) : %f, %f, %f", matI._41, matI._42, matI._43);
-//
-//			ImGui::TreePop();
-//		}
-//		ImGui::TreePop();
-//	}
-//}
-
 // 生成
 std::shared_ptr<Boss> Boss::Create()
 {
@@ -990,7 +542,7 @@ void Boss::AttackJudgment()
 }
 
 // ダメージ
-bool Boss::Damage(int damage)
+bool Boss::Damage(Phoenix::s32 damage)
 {
 	if (battleAI->GetCurrentStateName() == BattleEnemyState::Dedge) return false;
 
@@ -999,28 +551,29 @@ bool Boss::Damage(int damage)
 	if (damage <= 10)
 	{
 		life -= damage;
+		accumulationDamage += damage;
 
 		SetMoveInput(0.0f, 0.0f);
 		SetMoveSpeed(0.0f);
-
-		//battleAI->GoToState(BattleEnemyState::DamageSmall, true);
-
-		//changeAnimation = true;
-		//changeState = BattleEnemyState::DamageSmall;
 
 		downDamage = true;
 	}
 	else if (damage <= 20)
 	{
 		life -= damage;
+		accumulationDamage += damage;
 
-		SetMoveInput(0.0f, 0.0f);
-		SetMoveSpeed(0.0f);
+		// 蓄積ダメージの確認
+		if (!AccumulationDamage(damage))
+		{
+			SetMoveInput(0.0f, 0.0f);
+			SetMoveSpeed(0.0f);
 
-		battleAI->GoToState(BattleEnemyState::DamageBig, true);
+			battleAI->GoToState(BattleEnemyState::DamageBig, true);
 
-		changeAnimation = true;
-		changeState = BattleEnemyState::DamageBig;
+			changeAnimation = true;
+			changeState = BattleEnemyState::DamageBig;
+		}
 
 		downDamage = true;
 	}
@@ -1039,6 +592,29 @@ bool Boss::Damage(int damage)
 	}
 
 	return downDamage;
+}
+
+// 蓄積ダメージ
+bool Boss::AccumulationDamage(Phoenix::s32 damage)
+{
+	accumulationDamage += damage;
+
+	if (AccumulationMaxDamage <= accumulationDamage)
+	{
+		accumulationDamage = 0;
+
+		SetMoveInput(0.0f, 0.0f);
+		SetMoveSpeed(0.0f);
+
+		battleAI->GoToState(BattleEnemyState::KnockBack, true);
+
+		changeAnimation = true;
+		changeState = BattleEnemyState::KnockBack;
+
+		return true;
+	}
+
+	return false;
 }
 
 // プレイヤーに攻撃が当たる距離に入っているか？
--- Phoenix/Source/Boss/Boss.h
+++ Phoenix/Source/Boss/Boss.h
@@ -6,128 +6,11 @@
 #include "../UI/BossUI.h"
 
 
-//class Boss
-//{
-//private:
-//	/*static constexpr*/ Phoenix::s32 MaxLife = 1000; // TODO : 調整必須
-//	/*static constexpr*/ Phoenix::s32 AccumulationMaxDamege = 60; // TODO : 調整必須
-//	/*static constexpr*/ Phoenix::s32 AccumulationTime = 5 * 60;
-//
-//private:
-//	std::unique_ptr<Phoenix::FrameWork::ModelObject> model;
-//	std::unique_ptr<Phoenix::FrameWork::ModelObject> effectModel;
-//	std::unique_ptr<AI> ai;
-//	std::shared_ptr<BossUI> ui;
-//
-//	BossAI* bossAI = nullptr;
-//	Player* player = nullptr;
-//
-//	AIStateType currentType = AIStateType::None;
-//	AIStateType nextType = AIStateType::None;
-//
-//	Phoenix::Math::Matrix worldMatrix;
-//	Phoenix::Math::Vector3 pos;
-//	//Phoenix::Math::Vector3 rotate;
-//	Phoenix::Math::Quaternion rotate;
-//	Phoenix::Math::Vector3 scale;
-//	Phoenix::f32 radius;
-//	Phoenix::f32 speed;
-//
-//	Phoenix::u32 boneIndex = -1;
-//
-//	Phoenix::Math::Vector3 oldPos;
-//
-//	std::vector<Phoenix::FrameWork::CollisionData> collisionDatas;
-//
-//	// HP
-//	Phoenix::s32 life = 0;
-//
-//	// アタックが当たったか？
-//	bool isHit = false;
-//
-//	// アタックの判定中か？
-//	bool isAttackJudgment = false;
-//
-//	// コリジョンデータの要素数
-//	Phoenix::u32 attackCollisionIndex = 0;
-//
-//	// 蓄積ダメージ
-//	Phoenix::s32 accumulationDamege = 0;
-//	Phoenix::s32 accumulationTimeCnt = 0;
-//	bool isChangeAccumulationDamege = false;
-//
-//	// ディゾルブ
-//	bool isDissolve = false;
-//	Phoenix::f32 animCnt = 0.0f;
-//
-//	// ジャンプ攻撃スタート時
-//	bool isJumpAttackStart = false;
-//
-//	// ジャンプ攻撃にスタートを一度オンしたか確認
-//	bool onJumpAttackStart = false;
-//
-//public:
-//	Boss() :
-//		worldMatrix(Phoenix::Math::MatrixIdentity()),
-//		speed(0.0f),
-//		radius(0.0f) 
-//	{}
-//	~Boss() {}
-//
-//public:
-//	static std::unique_ptr<Boss> Create();
-//	void Construct(Phoenix::Graphics::IGraphicsDevice* graphicsDevice, Player* player);
-//	void Initialize();
-//	void Update(bool onControl);
-//	void UpdateUI();
-//	void ChangeAnimation(AIStateType type);
-//	void AttackJudgment();
-//	void GUI();
-//	void Damage(int damage)
-//	{
-//		life -= damage;
-//		if (!IsJumpAttack())
-//		{
-//			accumulationDamege += damage;
-//		}
-//	}
-//	void AccumulationDamege();
-//
-//	Phoenix::FrameWork::ModelObject* GetModel() { return model.get(); }
-//	Phoenix::FrameWork::ModelObject* GetEffectModel() { return effectModel.get(); }
-//	Phoenix::Math::Matrix GetWorldMatrix() { return worldMatrix; }
-//	Phoenix::Math::Vector3 GetPosition() { return pos; }
-//	//Phoenix::Math::Vector3 GetRotate() { return rotate; }
-//	Phoenix::Math::Quaternion GetRotate() { return rotate; }
-//	Phoenix::f32 GetRadius() { return radius; }
-//	Phoenix::s32 GetHP() { return life; }
-//	Phoenix::u32 GetBoneIndex() { return boneIndex; }
-//	std::vector<Phoenix::FrameWork::CollisionData>* GetCollisionDatas() { return &collisionDatas; }
-//	bool IsAttackJudgment() { return isAttackJudgment; }
-//	Phoenix::u32 GetAttackCollisionIndex() { return attackCollisionIndex; }
-//	BossUI* GetUI() { return ui.get(); }
-//	bool IsAccumulationDamege() { return (0.0f < accumulationDamege); }
-//	bool IsJumpAttack() { return (currentType == AIStateType::JumpAttack) ? true : false; }
-//	bool IsChangeAccumulationDamege()
-//	{
-//		if (!isChangeAccumulationDamege) return false;
-//
-//		isChangeAccumulationDamege = false;
-//		return true;
-//	}
-//	bool IsDissolve() { return isDissolve; }
-//	bool IsJumpAttackStart() { return isJumpAttackStart; }
-//
-//	void SetPosition(Phoenix::Math::Vector3 pos) { this->pos = pos; }
-//	//void SetRotate(Phoenix::Math::Vector3 rotate) { this->rotate = rotate; }
-//	void SetRotate(Phoenix::Math::Quaternion rotate) { this->rotate = rotate; }
-//	void SetIsHit(bool isHit) { this->isHit = isHit; }
-//};
-
 class Boss : public Enemy
 {
 public:
-	static constexpr Phoenix::s32 LifeRange = 1000;
+	static constexpr Phoenix::s32 LifeRange = 250;
+	static constexpr Phoenix::s32 AccumulationMaxDamage = 80 * 2;
 
 private:
 	// 攻撃ステート
@@ -174,7 +57,10 @@ public:
 	bool InDistanceHitByAttack() override;
 
 	// ダメージ
-	bool Damage(int damage) override;
+	bool Damage(Phoenix::s32 damage) override;
+
+	// 蓄積ダメージ
+	bool AccumulationDamage(Phoenix::s32 damage) override;
 
 public:
 	// 攻撃ステートを変更
